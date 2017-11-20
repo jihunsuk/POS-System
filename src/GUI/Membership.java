1108e4a2b7bc20019765ba60c0cbfda921eb81a6
석지훈 <tjrwlgns1198@gmail.com>
2017-11-20 21:31:01 +0900

    add button, label, textField

--- src/GUI/Membership.java
+++ src/GUI/Membership.java
@@ -3,10 +3,19 @@ package GUI;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class Membership {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
 
 	/**
 	 * Launch the application.
@@ -36,8 +45,68 @@ public class Membership {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setTitle("\uD68C\uC6D0\uAC00\uC785");
+		frame.setBounds(100, 100, 455, 305);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("\uC544\uC774\uB514");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(79, 49, 69, 15);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("\uBE44\uBC00\uBC88\uD638");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setBounds(79, 74, 69, 15);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("\uC774\uB984");
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_2.setBounds(79, 99, 69, 15);
+		frame.getContentPane().add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("\uB098\uC774");
+		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_3.setBounds(79, 124, 69, 15);
+		frame.getContentPane().add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("\uD734\uB300\uD3F0 \uBC88\uD638");
+		lblNewLabel_4.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_4.setBounds(79, 149, 69, 15);
+		frame.getContentPane().add(lblNewLabel_4);
+		
+		textField = new JTextField();
+		textField.setBounds(178, 46, 186, 21);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(178, 71, 186, 21);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(178, 96, 186, 21);
+		frame.getContentPane().add(textField_2);
+		textField_2.setColumns(10);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(178, 121, 186, 21);
+		frame.getContentPane().add(textField_3);
+		textField_3.setColumns(10);
+		
+		textField_4 = new JTextField();
+		textField_4.setBounds(178, 146, 186, 21);
+		frame.getContentPane().add(textField_4);
+		textField_4.setColumns(10);
+		
+		JButton Member_Enroll = new JButton("\uAC00\uC785\uD558\uAE30");
+		Member_Enroll.setBounds(112, 198, 97, 23);
+		frame.getContentPane().add(Member_Enroll);
+		
+		JButton Enroll_Cancle = new JButton("\uAC00\uC785\uCDE8\uC18C");
+		Enroll_Cancle.setBounds(237, 198, 97, 23);
+		frame.getContentPane().add(Enroll_Cancle);
 	}
 
 }

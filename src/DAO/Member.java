8dbb37eb167d85a928a9326ff3ad783624bd882d
isseebx123 <lsseebx38@gmail.com>
2017-11-20 22:22:38 +0900

    add Member fields, getter, setter

--- src/DAO/Member.java
+++ src/DAO/Member.java
@@ -1,6 +1,8 @@
 package DAO;
 
 public class Member {
+	private String ID, PWD, Name, Age, PhoneNumber;
+	
 	public Member() {
 
 	}
@@ -13,5 +15,45 @@ public class Member {
 	public boolean doMembership(final String ID, final String PWD, final String NAME, final String PHONENUMBER){
 		
 		return false;
+	}
+
+	public String getID() {
+		return ID;
+	}
+
+	public void setID(String iD) {
+		ID = iD;
+	}
+
+	public String getPWD() {
+		return PWD;
+	}
+
+	public void setPWD(String pWD) {
+		PWD = pWD;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public String getAge() {
+		return Age;
+	}
+
+	public void setAge(String age) {
+		Age = age;
+	}
+
+	public String getPhoneNumber() {
+		return PhoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		PhoneNumber = phoneNumber;
 	}
 }

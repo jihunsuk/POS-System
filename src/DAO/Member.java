630c3960d6e8120c617c53104ad429ef809ff647
isseebx123 <lsseebx38@gmail.com>
2017-11-20 22:12:21 +0900

    add Member proto methods

--- src/DAO/Member.java
+++ src/DAO/Member.java
@@ -4,4 +4,14 @@ public class Member {
 	public Member() {
 
 	}
+	
+	public boolean doLogin(final String ID, final String PWD){
+		
+		return false;
+	}
+	
+	public boolean doMembership(final String ID, final String PWD, final String NAME, final String PHONENUMBER){
+		
+		return false;
+	}
 }

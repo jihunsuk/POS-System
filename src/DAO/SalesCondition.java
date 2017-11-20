cac501e159ad58d193fae63828628952c8fc1680
isseebx123 <lsseebx38@gmail.com>
2017-11-20 22:43:04 +0900

    add SalesCondition methods, fields

--- src/DAO/SalesCondition.java
+++ src/DAO/SalesCondition.java
@@ -1,7 +1,25 @@
 package DAO;
 
 public class SalesCondition {
+	private int totalSalesMoney;
+	
 	public SalesCondition(){
 		
+	}
+
+	public void daySalesCheck(){
+		
+	}
+
+	public void monthSalesCheck(){
+		
+	}
+	
+	public int getTotalSalesMoney() {
+		return totalSalesMoney;
+	}
+
+	public void setTotalSalesMoney(int totalSalesMoney) {
+		this.totalSalesMoney = totalSalesMoney;
 	}
 }

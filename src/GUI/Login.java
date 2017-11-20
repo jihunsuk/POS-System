c3a539c841dbd18b295f6e77916829b28f309b9a
석지훈 <tjrwlgns1198@gmail.com>
2017-11-20 21:37:24 +0900

    correct var name

--- src/GUI/Login.java
+++ src/GUI/Login.java
@@ -8,12 +8,13 @@ import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import java.awt.Font;
+import javax.swing.SwingConstants;
 
 public class Login {
 
 	private JFrame frame;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField ID;
+	private JTextField PWD;
 
 	/**
 	 * Launch the application.
@@ -44,46 +45,48 @@ public class Login {
 	private void initialize() {
 		frame = new JFrame();
 		frame.setTitle("\uD3EC\uC2A4 \uC2DC\uC2A4\uD15C");
-		frame.setBounds(100, 100, 400, 287);
+		frame.setBounds(100, 100, 393, 254);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JButton btnNewButton = new JButton("\uB85C\uADF8\uC778");
-		btnNewButton.addActionListener(new ActionListener() {
+		JButton Login = new JButton("\uB85C\uADF8\uC778");
+		Login.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				//로그인 눌렀을 경우
 				
 			}
 		});
-		btnNewButton.setBounds(76, 163, 97, 23);
-		frame.getContentPane().add(btnNewButton);
+		Login.setBounds(76, 145, 97, 23);
+		frame.getContentPane().add(Login);
 		
-		JButton btnNewButton_1 = new JButton("\uD68C\uC6D0\uAC00\uC785");
-		btnNewButton_1.addActionListener(new ActionListener() {
+		JButton Membership = new JButton("\uD68C\uC6D0\uAC00\uC785");
+		Membership.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				//회원가입 눌렀을 경우
 				
 			}
 		});
-		btnNewButton_1.setBounds(203, 163, 97, 23);
-		frame.getContentPane().add(btnNewButton_1);
+		Membership.setBounds(203, 145, 97, 23);
+		frame.getContentPane().add(Membership);
 		
-		textField = new JTextField();
-		textField.setBounds(174, 71, 116, 21);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
+		ID = new JTextField();
+		ID.setBounds(174, 71, 116, 21);
+		frame.getContentPane().add(ID);
+		ID.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(174, 115, 116, 21);
-		frame.getContentPane().add(textField_1);
-		textField_1.setColumns(10);
+		PWD = new JTextField();
+		PWD.setBounds(174, 102, 116, 21);
+		frame.getContentPane().add(PWD);
+		PWD.setColumns(10);
 		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setBounds(96, 70, 66, 23);
+		JLabel lblNewLabel = new JLabel("\uB85C\uADF8\uC778");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(96, 72, 66, 18);
 		frame.getContentPane().add(lblNewLabel);
 		
-		JLabel lblNewLabel_1 = new JLabel("New label");
-		lblNewLabel_1.setBounds(96, 118, 57, 15);
+		JLabel lblNewLabel_1 = new JLabel("\uBE44\uBC00\uBC88\uD638");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setBounds(96, 103, 66, 18);
 		frame.getContentPane().add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("POS SYSTEM");

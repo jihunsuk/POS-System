9035b4fd1d722514e5e38dd4f2e0f17cf2f8434a
석지훈 <tjrwlgns1198@gmail.com>
2017-11-24 20:32:53 +0900

    correct Membership.java

--- src/DAO/Member.java
+++ src/DAO/Member.java
@@ -7,13 +7,16 @@ public class Member {
 
 	}
 	
-	public boolean doLogin(final String ID, final String PWD){
-		
+	public static boolean doLogin(final String ID, final String PWD){
+		//JOptionPane.showMessageDialog(null, "아이디가 존재하지 않습니다.");
+		//JOptionPane.showMessageDialog(null, "비밀번호가 일치하지 않습니다.");
+		//JOptionPane.showMessageDialog(null, "아이디와 비밀번호를 입력해주세요.");
 		return false;
 	}
 	
-	public boolean doMembership(final String ID, final String PWD, final String NAME, final String PHONENUMBER){
-		
+	public static boolean doMembership(final String ID, final String PWD, final String NAME, final String PHONENUMBER){
+		//JOptionPane.showMessageDialog(null, "중복된 ID입니다. 다시 입력해 주세요.’");
+		//JOptionPane.showMessageDialog(null, "입력되지 않은 정보가 있습니다. 다시 입력해 주세요.’");
 		return false;
 	}
 

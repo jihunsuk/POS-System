590a40da599746d1c5cd08508b50d584cfffb53e
isseebx123 <lsseebx38@gmail.com>
2017-12-15 23:36:44 +0900

    remove cal time code that using in SalesCondition

--- src/DAO/Payment.java
+++ src/DAO/Payment.java
@@ -9,17 +9,13 @@ public class Payment {
 	}
 	
 	public int doCashPay(final int totalPrice, final int receiveMoney){
-		long t = System.currentTimeMillis(); 
-		SimpleDateFormat dayTime = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
-		String time = dayTime.format(new Date(t));
+
 
 		return 0;
 	}
 	
 	public boolean doCreditPay(final int totalPrice){
-		long t = System.currentTimeMillis(); 
-		SimpleDateFormat dayTime = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
-		String time = dayTime.format(new Date(t));
+
 		
 		return false;
 	}

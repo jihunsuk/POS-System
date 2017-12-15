5b5195c451c9038e1d52e73c5be6b700c451636c
isseebx123 <lsseebx38@gmail.com>
2017-12-15 23:35:58 +0900

    init to addSales

--- src/DAO/SalesCondition.java
+++ src/DAO/SalesCondition.java
@@ -1,5 +1,8 @@
 package DAO;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class SalesCondition {
 	private int totalSalesMoney;
 	
@@ -12,6 +15,20 @@ public class SalesCondition {
 	}
 
 	public void monthSalesCheck(){
+		
+	}
+	
+	// 매출현황이 추가되는 것은 유스케이스가 아님.
+	// TODO: 결제가 완료되었을 때 GUI에서 호출하여 매출추가
+	// ex) GUI에서 ShoppingBasket s; 일때
+	// s.getAmount().entrySet();
+	// for(each entry) {
+	// 	SalesCondition.addSales(goodsName, amount);
+	// }
+	public static void addSales(String goodsName, int amount){
+		long t = System.currentTimeMillis(); 
+		SimpleDateFormat dayTime = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+		String time = dayTime.format(new Date(t));
 		
 	}
 	

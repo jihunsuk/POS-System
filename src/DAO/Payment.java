b48c255bdb159c8da08a2b508bb146867b96918c
isseebx123 <lsseebx38@gmail.com>
2017-12-15 23:22:02 +0900

    add code to calculating time

--- src/DAO/Payment.java
+++ src/DAO/Payment.java
@@ -1,16 +1,25 @@
 package DAO;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Payment {
 	public Payment(){
 		
 	}
 	
 	public int doCashPay(final int totalPrice, final int receiveMoney){
-		
+		long t = System.currentTimeMillis(); 
+		SimpleDateFormat dayTime = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+		String time = dayTime.format(new Date(t));
+
 		return 0;
 	}
 	
 	public boolean doCreditPay(final int totalPrice){
+		long t = System.currentTimeMillis(); 
+		SimpleDateFormat dayTime = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+		String time = dayTime.format(new Date(t));
 		
 		return false;
 	}

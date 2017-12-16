c01d13c31b5b993a160fd776d9110bc7d336bfdf
isseebx123 <lsseebx38@gmail.com>
2017-12-17 00:02:01 +0900

    implement salescondition

--- src/DAO/SalesCondition.java
+++ src/DAO/SalesCondition.java
@@ -3,7 +3,9 @@ package DAO;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
@@ -22,17 +24,19 @@ public class SalesCondition {
 	public void daySalesCheck() {
 		Connection c = null;
 		Statement stmt = null;
-
+		basketList.clear();
+		
+		SimpleDateFormat dayTime = new SimpleDateFormat("yyyyMMdd");
+		String thisDay = dayTime.format(new Date());
 		try {
 			c = DBConnection.getConnection();
 			stmt = c.createStatement();
 
-			String query = String.format("SELECT * from `possystem`.`order` order by orderNo desc");
+			String query = String.format("SELECT * from `possystem`.`order` where substring(`time`, 1, 8) = '%s' order by orderNo desc", thisDay);
 			ResultSet rs = stmt.executeQuery(query);
 
-			int basketIdx = 0;
 			int prevOrderNo = -1;
-			ShoppingBasket basket = null;
+			ShoppingBasket bsk = null;
 			while (rs.next()) {
 				String goodsName = rs.getString("goodsName");
 				String time = rs.getString("time");
@@ -41,13 +45,13 @@ public class SalesCondition {
 				int amount = rs.getInt("amount");
 
 				if (prevOrderNo != orderNo) {
-					basket = new ShoppingBasket();
-					date.put(basketIdx++, time);
+					bsk = new ShoppingBasket();
+					basketList.add(bsk);
+					date.put(orderNo, time);
 					prevOrderNo = orderNo;
 				}
 				
-				
-				
+				bsk.setTotalPrice(bsk.getTotalPrice() + (price * amount));
 			}
 
 			stmt.close();
@@ -58,8 +62,45 @@ public class SalesCondition {
 		}
 	}
 
-	public static void monthSalesCheck() {
+	public void monthSalesCheck() {
+		Connection c = null;
+		Statement stmt = null;
+		basketList.clear();
+		
+		SimpleDateFormat dayTime = new SimpleDateFormat("yyyyMM");
+		String thisMonth = dayTime.format(new Date());
+		try {
+			c = DBConnection.getConnection();
+			stmt = c.createStatement();
 
+			String query = String.format("SELECT * from `possystem`.`order` where substring(`time`, 1, 6) = '%s' order by orderNo desc", thisMonth);
+			ResultSet rs = stmt.executeQuery(query);
+
+			int prevOrderNo = -1;
+			ShoppingBasket bsk = null;
+			while (rs.next()) {
+				String goodsName = rs.getString("goodsName");
+				String time = rs.getString("time");
+				int orderNo = rs.getInt("orderNo");
+				int price = rs.getInt("price");
+				int amount = rs.getInt("amount");
+
+				if (prevOrderNo != orderNo) {
+					bsk = new ShoppingBasket();
+					basketList.add(bsk);
+					date.put(orderNo, time);
+					prevOrderNo = orderNo;
+				}
+				
+				bsk.setTotalPrice(bsk.getTotalPrice() + (price * amount));
+			}
+
+			stmt.close();
+			System.out.println("조회에 성공했습니다.");
+		} catch (Exception e) {
+			System.out.println("조회에 실패했습니다.");
+			e.printStackTrace();
+		}
 	}
 
 	public int getTotalSalesMoney() {
@@ -68,5 +109,13 @@ public class SalesCondition {
 
 	public void setTotalSalesMoney(int totalSalesMoney) {
 		this.totalSalesMoney = totalSalesMoney;
+	}
+	
+	public HashMap<Integer, String> getDate(){
+		return this.date;
+	}
+	
+	public List<ShoppingBasket> getBasketList(){
+		return this.basketList;
 	}
 }
